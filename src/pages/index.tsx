e0367d38b8489dfd3f71f29d7c7670d1e8ee5fbd
Catmily <emily.chomicz0@gmail.com>
2022-01-31 20:13:45 +0000

    Hide items and show construction unless authenticated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import PreviewGrid from "../components/preview-grid";
 import styles from "../styles/Home.module.scss";
 import getArticles from "../mocks/articles";
 import useArticle from "../hooks/article";
 import ConstructionModal from "../components/construction-modal";
 import SearchBar from "../components/search"
+import { User } from "oidc-client";
+import { AuthContext } from "../providers/AuthProvider";
 
 export default function Home() {
   const articles = getArticles();
   const reactContent = useArticle("homepage");
 
+  const [user, setUser] = useState<User>();
+
+  const authService = useContext(AuthContext);
+
+  useEffect(() => {
+    authService.getUser().then((user) => setUser(user));
+  }, []);
+
+  const idToken = useMemo(() => user?.id_token, [user])
+  
   return (
     <>
       <div className={styles.content}>
@@ -17,7 +29,7 @@ export default function Home() {
       <SearchBar />
       </div>
       <PreviewGrid articles={articles} />
-      <ConstructionModal />
+      if (!user) <ConstructionModal />
     </>
   );
 }
